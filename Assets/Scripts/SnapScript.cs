1680c77724ba7e2ba0b97db23a691ad0a62525f1
larry2toes <64716723+larry2toes@users.noreply.github.com>
2021-01-07 14:03:53 -0600

    Selection and moving undone
    ToDo - make distance from cam reset when unsnapped

--- Assets/Scripts/SnapScript.cs
+++ Assets/Scripts/SnapScript.cs
@@ -6,7 +6,10 @@ using UnityEngine;
 public class SnapScript : MonoBehaviour
 {
     [SerializeField] int gridSize = 5;
+    [SerializeField] [Range(0, 100)] int snapDist;
 
+
+    public bool isSnapped = false;
     private void Update() {
         snapToGrid();
     }
@@ -28,5 +31,96 @@ public class SnapScript : MonoBehaviour
             Mathf.RoundToInt(transform.position.z / gridSize)
             );
         ;
+    }
+
+    private void OnMouseDrag() {
+        TransformObj();
+
+        SnapToObj();
+    }
+
+    private void TransformObj() {
+        Vector3 newPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,
+                    Mathf.Abs(Camera.main.transform.position.z - transform.position.z)));
+        newPos.z = transform.position.z;
+
+        transform.position = newPos;
+    }
+
+    private void SnapToObj() {
+        Vector3 rayDir = transform.position - Camera.main.transform.position;
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.transform.position, rayDir, out hit, snapDist)) {
+
+            Debug.DrawRay(Camera.main.transform.position, rayDir * hit.distance, Color.green);
+            isSnapped = false;
+
+
+            if (hit.collider.name == "Cube") {
+                Debug.DrawRay(Camera.main.transform.position, rayDir * hit.distance, Color.blue);
+                Vector3 normal = hit.normal;
+                normal = hit.transform.TransformDirection(normal);
+                if (normal == hit.transform.up) {
+                    transform.position = new Vector3(
+                        hit.collider.transform.position.x,
+                        hit.collider.transform.position.y + 1,
+                        hit.collider.transform.position.z
+                    );
+                    ;
+                }
+                if (normal == -hit.transform.up) {
+                    transform.position = new Vector3(
+                        hit.collider.transform.position.x,
+                        hit.collider.transform.position.y - 1,
+                        hit.collider.transform.position.z
+                    );
+                    ;
+                }
+                if (normal == hit.transform.right) {
+                    transform.position = new Vector3(
+                        hit.collider.transform.position.x + 1,
+                        hit.collider.transform.position.y,
+                        hit.collider.transform.position.z
+                    );
+                    ;
+                }
+                if (normal == -hit.transform.right) {
+                    transform.position = new Vector3(
+                        hit.collider.transform.position.x - 1,
+                        hit.collider.transform.position.y,
+                        hit.collider.transform.position.z
+                    );
+                    ;
+                }
+                if (normal == hit.transform.forward) {
+                    transform.position = new Vector3(
+                        hit.collider.transform.position.x,
+                        hit.collider.transform.position.y,
+                        hit.collider.transform.position.z + 1
+                    );
+                    ;
+                }
+                if (normal == -hit.transform.forward) {
+                    transform.position = new Vector3(
+                        hit.collider.transform.position.x,
+                        hit.collider.transform.position.y,
+                        hit.collider.transform.position.z - 1
+                    );
+                    ;
+                }
+                isSnapped = true;
+            }
+
+        } else {
+            Debug.DrawRay(Camera.main.transform.position, rayDir * snapDist, Color.red);
+            isSnapped = false;
+        }
+
+        if (isSnapped) {
+            transform.position = Camera.main.transform.position - transform.position/2;
+            isSnapped = false;
+        }
+
+
     }
 }
